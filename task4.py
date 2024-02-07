176fe0b621e314576683175cef41dc995ba0136a
Svyatoslav Dzyadevych <svyatoslav.dzyadevych@gmail.com>
2024-02-07 23:46:45 +0200

    Completed task #4

--- task4.py
+++ task4.py
@@ -1,31 +1,34 @@
+import heapq
 import uuid
 import networkx as nx
 import matplotlib.pyplot as plt
-import heapq
 
 
 class Node:
     def __init__(self, key, color="skyblue"):
-        self.val = key
-        self.color = color
-        self.id = str(uuid.uuid4())
         self.left = None
         self.right = None
+        self.val = key
+        self.color = color  # Додатковий аргумент для зберігання кольору вузла
+        # Унікальний ідентифікатор для кожного вузла
+        self.id = str(uuid.uuid4())
 
 
 def add_edges(graph, node, pos, x=0, y=0, layer=1):
     if node is not None:
+        # Використання id та збереження значення вузла
         graph.add_node(node.id, color=node.color, label=node.val)
         if node.left:
             graph.add_edge(node.id, node.left.id)
             l = x - 1 / 2 ** layer
             pos[node.left.id] = (l, y - 1)
-            add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
         if node.right:
             graph.add_edge(node.id, node.right.id)
             r = x + 1 / 2 ** layer
             pos[node.right.id] = (r, y - 1)
-            add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+            r = add_edges(graph, node.right, pos, x=r,
+                          y=y - 1, layer=layer + 1)
     return graph
 
 
@@ -35,7 +38,8 @@ def draw_tree(tree_root):
     tree = add_edges(tree, tree_root, pos)
 
     colors = [node[1]['color'] for node in tree.nodes(data=True)]
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(
+        data=True)}  # Використовуйте значення вузла для міток
 
     plt.figure(figsize=(8, 5))
     nx.draw(tree, pos=pos, labels=labels, arrows=False,
@@ -73,6 +77,6 @@ def draw_heap(arr):
 
 
 if __name__ == "__main__":
-    arr = [15, 10, 8, 12, 4, 7, 6, 3, 2]
+    arr = [0, 4, 1, 5, 10, 3]
     heapq.heapify(arr)
     draw_heap(arr)
