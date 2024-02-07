138d3b0a4f602ed2d81703bb33e443fef81eeaf1
Svyatoslav Dzyadevych <svyatoslav.dzyadevych@gmail.com>
2024-02-07 23:25:33 +0200

    Completed task #3

--- task3.py
+++ task3.py
@@ -1,45 +1,45 @@
-import heapq
+from networkx import Graph
+from heapq import heappush, heappop
 
+def dijkstra3(graph, start):
+    distances = {vertex: float('infinity') for vertex in graph.nodes}
+    distances[start] = 0
 
-class Graph:
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = {i: [] for i in range(self.V)}
+    priority_queue = [(0, start)]
 
-    def add_edge(self, u, v, w):
-        self.graph[u].append((v, w))
+    while priority_queue:
+        current_distance, current_vertex = heappop(priority_queue)
 
-    def dijkstra(self, src):
-        min_heap = [(0, src)]
-        distances = [float('inf')] * self.V
-        distances[src] = 0
+        if current_distance > distances[current_vertex]:
+            continue
 
-        while min_heap:
-            dist, u = heapq.heappop(min_heap)
+        for neighbor in graph.neighbors(current_vertex):
+            weight = graph[current_vertex][neighbor]['weight']
+            distance = current_distance + weight
 
-            for neighbor, weight in self.graph[u]:
-                if distances[u] + weight < distances[neighbor]:
-                    distances[neighbor] = distances[u] + weight
-                    heapq.heappush(min_heap, (distances[neighbor], neighbor))
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
 
-        return distances
+                heappush(priority_queue, (distance, neighbor))
+
+    return distances
 
 
 if __name__ == "__main__":
-    g = Graph(9)
-    g.add_edge(0, 1, 4)
-    g.add_edge(0, 7, 8)
-    g.add_edge(1, 2, 8)
-    g.add_edge(1, 7, 11)
-    g.add_edge(2, 3, 7)
-    g.add_edge(2, 8, 2)
-    g.add_edge(2, 5, 4)
-    g.add_edge(3, 4, 9)
-    g.add_edge(3, 5, 14)
-    g.add_edge(4, 5, 10)
-    g.add_edge(5, 6, 2)
-    g.add_edge(6, 7, 1)
-    g.add_edge(6, 8, 6)
-    g.add_edge(7, 8, 7)
+    G = Graph()
 
-    print(g.dijkstra(0))
+    edges_with_weights = [
+        ("A", "B", 1), ("A", "C", 3), ("A", "D", 7),
+        ("B", "E", 5), ("C", "F", 4), ("D", "G", 2),
+        ("E", "F", 6), ("F", "G", 3), ("G", "H", 1),
+        ("H", "I", 2), ("I", "J", 3), ("J", "E", 4),
+        ("E", "H", 5)
+    ]
+
+    for edge in edges_with_weights:
+        G.add_edge(edge[0], edge[1], weight=edge[2])
+
+    paths = {vertex: dijkstra3(G, vertex) for vertex in G.nodes}
+
+    for k, v in paths.items():
+        print(f"From vertex {k}:\n\tDistances : {v}\n")
