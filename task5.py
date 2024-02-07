34909f24c6b28e8049d19dfaa308c3382fc6b4bc
Svyatoslav Dzyadevych <svyatoslav.dzyadevych@gmail.com>
2024-02-08 01:06:37 +0200

    Completed task #5

--- task5.py
+++ task5.py
@@ -1,73 +1,86 @@
+import uuid
+
 import networkx as nx
 import matplotlib.pyplot as plt
-import uuid
 
 
 class Node:
-    def __init__(self, key):
-        self.val = key
-        self.id = str(uuid.uuid4())
+    def __init__(self, key, color="skyblue"):
         self.left = None
         self.right = None
-        self.color = "#FFFFFF"
+        self.val = key
+        self.color = color  # Додатковий аргумент для зберігання кольору вузла
+        # Унікальний ідентифікатор для кожного вузла
+        self.id = str(uuid.uuid4())
 
 
-def add_edges(graph, node, pos, colors, x=0, y=0, layer=1):
-    if node:
-        graph.add_node(node.id, label=node.val, color=node.color)
-        colors.append(node.color)
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        # Використання id та збереження значення вузла
+        graph.add_node(node.id, color=node.color, label=node.val)
         if node.left:
             graph.add_edge(node.id, node.left.id)
             l = x - 1 / 2 ** layer
             pos[node.left.id] = (l, y - 1)
-            add_edges(graph, node.left, pos, colors,
-                      x=l, y=y - 1, layer=layer + 1)
+            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
         if node.right:
             graph.add_edge(node.id, node.right.id)
             r = x + 1 / 2 ** layer
             pos[node.right.id] = (r, y - 1)
-            add_edges(graph, node.right, pos, colors,
-                      x=r, y=y - 1, layer=layer + 1)
-    return graph, colors
+            r = add_edges(graph, node.right, pos, x=r,
+                          y=y - 1, layer=layer + 1)
+    return graph
 
 
-def draw_tree(tree_root, title="Binary Tree"):
+def draw_tree(tree_root):
     tree = nx.DiGraph()
     pos = {tree_root.id: (0, 0)}
-    colors = []
-    tree, colors = add_edges(tree, tree_root, pos, colors)
-    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}
+    tree = add_edges(tree, tree_root, pos)
 
-    plt.figure(figsize=(12, 8))
-    plt.title(title)
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(
+        data=True)}  # Використовуйте значення вузла для міток
+
+    plt.figure(figsize=(8, 5))
     nx.draw(tree, pos=pos, labels=labels, arrows=False,
-            node_size=2500, node_color=colors)
+            node_size=2500, node_color=colors, font_color="red")
     plt.show()
 
 
-def dfs(node, visit_func, level=0):
+def color_get(cs):
+    r, g, b = [int(255 - (255 / (cs+1))) for _ in range(3)]
+    return f"#{r:02x}{g:02x}{b:02x}"
+
+
+def dfs(node, cs=0):
     if node:
-        node.color = visit_func(level)
-        dfs(node.left, visit_func, level + 1)
-        dfs(node.right, visit_func, level + 1)
+        #print(f"{cs}: {node.val}")
+
+        node.color = color_get(cs)
+        if node.left:
+            cs = dfs(node.left, cs + 2)
+        if node.right:
+            cs = dfs(node.right, cs + 2)
+
+    return cs
 
 
-def bfs(root, visit_func):
-    queue = [(root, 0)]
+def bfs(root):
+    queue = [root]
+    cs = 0
     while queue:
-        node, level = queue.pop(0)
+        node = queue.pop(0)
         if node:
-            node.color = visit_func(level)
-            queue.append((node.left, level + 1))
-            queue.append((node.right, level + 1))
+            #print(f"{cs}: {node.val}")
+
+            node.color = color_get(cs)
+            cs += 2
+
+            queue.append(node.left)
+            queue.append(node.right)
 
 
-def generate_color(level):
-    intensity = 255 - min(level * 30, 255)
-    return f"#{intensity:02x}{intensity:02x}FF"
-
-
-if __name__ == "__main__":
+def tree_get():
     root = Node(0)
     root.left = Node(4)
     root.left.left = Node(5)
@@ -75,8 +88,17 @@ if __name__ == "__main__":
     root.right = Node(1)
     root.right.left = Node(3)
 
-    dfs(root, generate_color)
-    draw_tree(root, "DFS Binary Tree")
+    return root
 
-    bfs(root, generate_color)
-    draw_tree(root, "BFS Binary Tree")
+
+if __name__ == "__main__":
+    root = tree_get()
+    draw_tree(root)
+
+    root = tree_get()
+    dfs(root)
+    draw_tree(root)
+
+    root = tree_get()
+    bfs(root)
+    draw_tree(root)
